331779d395ac31c8ad0c8ba511cca5aa43457a6f
Timanttiso <n.asujamaa@gmail.com>
2026-02-04 18:52:40 +0200

    Fixed knexfile

--- backend/knexfile.mjs
+++ backend/knexfile.mjs
@@ -1,73 +1,8 @@
-<<<<<<< HEAD
-// Update with your config settings.
-
-import "dotenv/config"
-=======
-// The following command runs any missing migrations inside the db/migrations folder:
-// npx knex migrate:latest 
-
-// the knex_migrations table is created automatically to keep track of the applied migrations
-
 import 'dotenv/config'
->>>>>>> 1f9c322d22265e093b51351361a8df3d78dfe0c2
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-<<<<<<< HEAD
-
-
-  export const development = {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
-   export const staging = {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
-
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
-  export const production =  {
-    client: 'postgresql',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
-
-=======
 export const development = {
   client: 'postgresql',
   connection: {
@@ -115,4 +50,3 @@ export const production = {
     tableName: 'knex_migrations'
   }
 };
->>>>>>> 1f9c322d22265e093b51351361a8df3d78dfe0c2
