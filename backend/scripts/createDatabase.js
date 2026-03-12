d2e7bc19da20762e57263643e7575c501e279653
hiru <siefuty@outlook.com>
2026-03-12 21:13:54 +0200

    Had to add this line otherwise /npm install fails. I'm not sure if this will make it not working on other's devices.

--- backend/scripts/createDatabase.js
+++ backend/scripts/createDatabase.js
@@ -18,6 +18,7 @@ async function createDatabase() {
         user: process.env.DB_USER || 'postgres',
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
         database: 'postgres'
     })
 
